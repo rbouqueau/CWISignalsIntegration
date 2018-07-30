f482ca1dbe9f1935d7f42a672ab9f1558dd61bf6
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2018-07-30 12:55:23 +0200

    remove useless DISABLE_PLY_LOADER

--- src/apps/pcl2dash/multi_file_reader.hpp
+++ src/apps/pcl2dash/multi_file_reader.hpp
@@ -6,8 +6,6 @@
 #include <cwi_encode/cwi_encode.h>
 #include <string>
 
-//#define DISABLE_PLY_LOADER
-
 class MultifileReader : public Modules::ActiveModule {
 public:
 	MultifileReader(const std::string &path)
@@ -18,29 +16,14 @@ public:
 	~MultifileReader() {}
 
 	bool work() override {
+		Tools::Profiler profiler("Processing PCC frame");
+
 		//TODO: void getPointCloud(long *netTimestamp, long *captureTimestamp, void *frame);
 
-		Tools::Profiler profiler("Processing PCC frame");
-#ifdef DISABLE_PLY_LOADER
-		auto p = format("%s", path);
-		auto file = fopen(p.c_str(), "rb");
-		if (!file) {
-			log(Warning, "Can't open file \"%s\" for reading. Exiting.", p);
-			return false;
-		}
-		fseek(file, 0, SEEK_END);
-		auto const size = ftell(file);
-		fseek(file, 0, SEEK_SET);
-
-		auto out = output->getBuffer(size);
-		fread(out->data(), 1, (size_t)out->size(), file);
-		fclose(file);
-#else
 		void *res = nullptr;
 		load_ply_file_XYZRGB(path, &res);
 		auto out = output->getBuffer(sizeof(res));
 		memcpy(out->data(), &res, sizeof(res));
-#endif
 		out->setMediaTime(fractionToClock(g_SystemClock->now()));
 		g_SystemClock->sleep(Fraction(1, 100)); //FIXME
 		output->emit(out);
